2d26997550ffa250cb3dcad3062a076bfd47e39b
Anna Ludwin <a.ludwin@getprintbox.com>
2024-11-14 13:59:55 +0100

    navigationButtons

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,67 @@ import './reset.css'
 import './styles.css'
 const NavigationButton = ({elementId, msg}) => <button id={elementId} className={"navigation"}>{msg}</button>
 
+const days = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"]
+const meals = ["Śniadanie", "Drugie śniadanie", "Obiad", "Podwieczorek", "Kolacja"]
 
-const DayPannel = ({dayName}) => {
+
+const DayPannel = ({dayName, children}) => {
     return <section className="day-panel">
         <h3>{dayName}</h3>
-        <div className="meal-list">
-        </div>
+        {children}
     </section>
 }
 
-function App() {
+const MealList = ({mealName, children}) => {
+    return <div className="meal-list">
+        <div className="meal-card">
+            <h4>{mealName}</h4>
+            <div className="content"></div>
+            <div className="recipe"></div>
+            <button className="more">(...)</button>
+            <div className="addition">
+                <hr/>
+                <h4 className="rail --spread">
+                    <span>dodatek</span>
+                    <button className="edit2">📋</button>
+                </h4>
+                <div className="content"></div>
+                <div className="recipe"></div>
+                <button className="more">(...)</button>
+            </div>
+            <button className="edit">📋</button>
+            {children}
+        </div>
+    </div>
+}
 
+
+function App() {
 
     return (
         <>
-
             <nav>
                 <h1>🔔BeL</h1>
                 <div className="rail">
-                    <NavigationButton elementId={`satMonButton`} msg={`sobota-poniedziałek`}></NavigationButton>
-                    <NavigationButton elementId={`tueFriButton`} msg={`wtorek-piątek`}></NavigationButton>
+                    <button id={`satMonButton`}>sobota-poniedziałek</button>
+                    <button id={`tueFriButton`}>wtorek-piątek</button>
                     <button>🍔</button>
                 </div>
             </nav>
 
-            <DayPannel dayName={`SOBOTA`}></DayPannel>
+            <div id="satMon" className="screen">{
+                days.map((day, index) => (
+                    <DayPannel dayName={days[index].toUpperCase()}>{
+                        meals.map((meal, index) => (
+                            <MealList mealName={meals[index]}></MealList>
+                        ))
+                    }
+                    </DayPannel>
+                ))
+            }
 
 
-
+            </div>
         </>
     )
 }
