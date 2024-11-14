c3b327adc56eb7d91f47e65f98fe5e8be2d63b75
Anna Ludwin <a.ludwin@getprintbox.com>
2024-11-14 15:20:37 +0100

    dayList with component

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,38 @@ const NavigationButton = ({elementId, msg}) => <button id={elementId} className=
 
 const days = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"]
 const meals = ["Śniadanie", "Drugie śniadanie", "Obiad", "Podwieczorek", "Kolacja"]
-const koktajl =
+const koktajl = {"sniadanie": [
+
     {
-    "title": "Koktajl z masła orzechowego, banana i siemienia lnianego",
-    "calories": "299 kcal",
-    "ingredients": [
-        "Mleko 79 g (0,4 szkl.),",
-        "Jogurt naturalny 86 g (0,6 opak. po 150 g),",
-        "Banan 86 g (0,5 średniej szt.),",
-        "Masło orzechowe 100% 14 g (niecała łyżka),",
-        "Nasiona słonecznika, dyni, chia, sezam lub siemię lniane 7 g (1,4 łyżeczki)"
-    ],
-    "reciepe": [
-        "Banana obierz i podziel na mniejsze kawałki",
-        "Wszystkie składniki umieść w pojemniku blendera",
-        "Zmiksuj na gładki koktajl"
-    ]
-}
+        "title": "Koktajl z masła orzechowego, banana i siemienia lnianego",
+        "calories": "299 kcal",
+        "ingredients": [
+            "Mleko 79 g (0,4 szkl.),",
+            "Jogurt naturalny 86 g (0,6 opak. po 150 g),",
+            "Banan 86 g (0,5 średniej szt.),",
+            "Masło orzechowe 100% 14 g (niecała łyżka),",
+            "Nasiona słonecznika, dyni, chia, sezam lub siemię lniane 7 g (1,4 łyżeczki)"
+        ],
+        "reciepe": [
+            "Banana obierz i podziel na mniejsze kawałki",
+            "Wszystkie składniki umieść w pojemniku blendera",
+            "Zmiksuj na gładki koktajl"
+        ]
+    }
+]}
+
+const salatka =
+    {
+        "title": "Sałatka pomidorowo-paprykowa",
+        "calories": "dodatek",
+        "ingredients": [
+            "Pomidor 130 g (1 średnia szt.),",
+            "Papryka 120 g (½ szt.),",
+            "Szczypiorek świeży lub suszony 2 g (1 szczypta)"],
+        "reciepe": [
+            "Pokrój pomidora i paprykę",
+            "Polej sosem winegret i posyp posiekanym szczypiorkiem"]
+    }
 
 const DayPannel = ({dayName, children}) => {
     return <section className="day-panel">
@@ -35,22 +50,17 @@ const MealList = ({mealName, children}) => {
         <div className="meal-card">
             <h4>{mealName}</h4>
 
-            <MealContent mealArray={koktajl}></MealContent>
+            // danie główne
+            <MealContent mealArray={koktajl.sniadanie[0]}></MealContent>
             <button className="more">(...)</button>
-            <MealRecipe mealArray={koktajl}></MealRecipe>
-            <div className="addition">
-                <hr/>
-                <h4 className="rail --spread">
-                    <span>dodatek</span>
-                    <button className="edit2">📋</button>
-                </h4>
-                <div className="content"></div>
-                <div className="recipe"></div>
-                <button className="more">(...)</button>
-            </div>
+            <MealRecipe mealArray={koktajl.sniadanie[0]}></MealRecipe>
+
+            // dodatki
+            <MealContent mealArray={salatka}></MealContent>
+            <button className="more">(...)</button>
+            <MealRecipe mealArray={salatka}></MealRecipe>
+
             <button className="edit">📋</button>
-
-
 
             {children}
         </div>
@@ -85,12 +95,13 @@ return <div className="recipe">
 }
 
 
+
 function App() {
 
     return (
         <>
             <nav>
-                <h1>🔔BeL</h1>
+            <h1>🔔BeL</h1>
                 <div className="rail">
                     <button id={`satMonButton`}>sobota-poniedziałek</button>
                     <button id={`tueFriButton`}>wtorek-piątek</button>
