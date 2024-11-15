8e060e77cececc0767578a2d09b3627681449d5e
Anna Ludwin <a.ludwin@getprintbox.com>
2024-11-15 11:41:42 +0100

    shoppingList

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,144 @@
-import './reset.css'
-import './styles.css'
-import './mealBase'
+import "./reset.css";
+import "./styles.css";
+import "./mealBase";
 import mealBase from "./mealBase";
 
-const daysSatMon = ["Sobota", "Niedziela", "Poniedziałek"]
-const daysTueFri = ["Wtorek", "Środa", "Czwartek", "Piątek"]
-const meals = ["Śniadanie", "Drugie śniadanie", "Obiad", "Podwieczorek", "Kolacja"]
-const addition = mealBase.dodatki[8]
-const mainCourse = mealBase.sniadanie.owsianki[1]
+const daysSatMon = ["Sobota", "Niedziela", "Poniedziałek"];
+const daysTueFri = ["Wtorek", "Środa", "Czwartek", "Piątek"];
+const meals = [
+  "Śniadanie",
+  "Drugie śniadanie",
+  "Obiad",
+  "Podwieczorek",
+  "Kolacja",
+];
+const addition = mealBase.dodatki[8];
+const mainCourse = mealBase.sniadanie.owsianki[1];
 
-
-const DayPannel = ({dayName, children}) => {
-    return <section className="day-panel">
-        <h3>{dayName}</h3>
-        {children}
+const DayPanel = ({ dayName, children }) => {
+  return (
+    <section className="day-panel">
+      <h3>{dayName}</h3>
+      {children}
     </section>
-}
+  );
+};
 
-const MealList = ({mealName, children}) => {
-    return <div className="meal-list">
-        <div className="meal-card">
-            <h4>{mealName}</h4>
+const MealList = ({ mealName }) => {
+  return (
+    <div className="meal-list">
+      <div className="meal-card">
+        <h4>{mealName}</h4>
 
-            <MealContent mealArray={mainCourse}></MealContent>
-            <button className="more">(...)</button>
-            <MealRecipe mealArray={mainCourse}></MealRecipe>
+        <MealContent mealArray={mainCourse}></MealContent>
+        <button className="more">(...)</button>
+        <MealRecipe mealArray={mainCourse}></MealRecipe>
 
-            <MealContent mealArray={addition}></MealContent>
-            <button className="more">(...)</button>
-            <MealRecipe mealArray={addition}></MealRecipe>
+        <MealContent mealArray={addition}></MealContent>
+        <button className="more">(...)</button>
+        <MealRecipe mealArray={addition}></MealRecipe>
 
-            <button className="edit">📋</button>
-
-            {children}
-        </div>
+        <button className="edit">📋</button>
+      </div>
     </div>
-}
+  );
+};
 
-const MealContent = ({mealArray}) => {
-    return <div className="content">
-        <p className="title">{mealArray.title}</p>
-        <h5>Składniki</h5>
-        <ul>{
-            mealArray.ingredients.map((ingredient) => (
-                <li>{ingredient}</li>
-            ))
-            }
-        </ul>
+const MealContent = ({ mealArray }) => {
+  return (
+    <div className="content">
+      <p className="title">{mealArray.title}</p>
+      <h5>Składniki</h5>
+      <ul>
+        {mealArray.ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
     </div>
-}
+  );
+};
 
-const MealRecipe = ({mealArray}) => {
-return <div className="recipe">
-    <h5>Przepis</h5>
-    <ol>{
-        mealArray.reciepe.map((ingredient) => (
-            <li>{ingredient}</li>
-        ))
-    }
-    </ol>
-    <button className="less">mniej</button>
-</div>
-}
+const MealRecipe = ({ mealArray }) => {
+  return (
+    <div className="recipe">
+      <h5>Przepis</h5>
+      <ol>
+        {mealArray.reciepe.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ol>
+      <button className="less">mniej</button>
+    </div>
+  );
+};
 
+const ShoppingListItem = ({ product }) => {
+  return (
+    <div>
+      <input type="checkbox" />
+      <em>{product}</em>
+    </div>
+  );
+};
+
+const WeekPanel = ({ weekPart, children }) => {
+  return (
+    <div id="satMon" className="screen">
+      {children}
+    </div>
+  );
+};
 
 function App() {
+  return (
+    <>
+      <nav>
+        <h1>🔔BeL</h1>
+        <div className="rail">
+          <button>sobota-poniedziałek</button>
+          <button>wtorek-piątek</button>
+          <button>🍔</button>
+        </div>
+      </nav>
 
-    return (
-        <>
-            <nav>
-            <h1>🔔BeL</h1>
-                <div className="rail">
-                    <button id={`satMonButton`}>sobota-poniedziałek</button>
-                    <button id={`tueFriButton`}>wtorek-piątek</button>
-                    <button>🍔</button>
-                </div>
-            </nav>
+      <div id="satMon" className="screen">
+        {daysSatMon.map((day) => (
+          <DayPanel key={day} dayName={day.toUpperCase()}>
+            {meals.map((meal) => (
+              <MealList key={meal} mealName={meal}></MealList>
+            ))}
+          </DayPanel>
+        ))}
 
-            <div id="satMon" className="screen">{
-                daysSatMon.map((day, index) => (
-                    <DayPannel dayName={daysSatMon[index].toUpperCase()}>{
-                        meals.map((meal, index) => (
-                            <MealList mealName={meals[index]}></MealList>
-                        ))
-                    }
-                    </DayPannel>
-                ))
-            }
+        <section id="shoppingListSatMon" className="day-panel dark">
+          <h3 className="shoppingList no-margin ">Lista zakupów</h3>
+          <h4 className="shoppingList">sobota-poniedziałek</h4>
+          <button className="shop">stwórz listę zakupów</button>
+          <div
+            className="hide"
+            style={{ color: "blanchedalmond", marginTop: "1em" }}
+          >
+            <input type="checkbox" value="hide" />
+            <label>ukryj zaznaczone</label>
+          </div>
 
-
-            </div>
-        </>
-    )
+          <div className="meal-card list">
+            <ShoppingListItem product={"Produkt 1"}></ShoppingListItem>
+            <ShoppingListItem product={"Produkt 2"}></ShoppingListItem>
+            <ShoppingListItem product={"..."}></ShoppingListItem>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
-export default App
+/*
+todo:
+ - przerzucic całość widoku do komponentu który przyjmuje argument - tablica dni
+ - dopisac eventHandler dl guzików przełaczania grupy dni(l. 81,82) - wyciągnąć na zewnąrz potem tę funkcję - dac ja powyzej returna z l. 75
+ - dopisac useState do tego przełączania
+
+
+*/
+export default App;
